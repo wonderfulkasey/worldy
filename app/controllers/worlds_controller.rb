36b3195e491577f47762b7655f850a0a748717aa
wonderfulkasey <kaseyarnold15@gmail.com>
2020-10-12 11:46:11 -0400

    added root site

--- app/controllers/worlds_controller.rb
+++ app/controllers/worlds_controller.rb
@@ -1,2 +1,3 @@
 class WorldsController < ApplicationController
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-# root ''
+root 'worlds#home'
 
 resources :worlds
 resources :plots
