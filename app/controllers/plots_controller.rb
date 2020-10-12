853cc1352fc90dce712a7505c5643c7b327b9f9f
wonderfulkasey <kaseyarnold15@gmail.com>
2020-10-12 11:51:01 -0400

    added to plots controller

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -1,2 +1,42 @@
 class PlotsController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    def new
+        @plot = @world.plots.build
+    end
+
+    def create
+    end
+
+    def edit
+       @plot = Plot.find(params[:id])
+    end
+
+    def update
+    end
+
+    def destroy
+        @plot = Plot.find(params[:id])
+        @plot.destroy
+
+        redirect_to plots_path 
+    end
+
+    private 
+
+    def plot_params
+        params.require(:plot).permit(
+            :title,
+            :importance,
+            :description,
+            :user_id,
+            :world_id
+        )
+    end
+
 end
