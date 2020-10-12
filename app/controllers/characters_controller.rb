06a931bc90e4c1c06651b1b70a669b44bd3bb338
wonderfulkasey <kaseyarnold15@gmail.com>
2020-10-12 11:52:51 -0400

    adding to character controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,45 @@
 class CharactersController < ApplicationController
+
+    def index
+        redirect_to worlds_path(@worlds)
+    end
+
+    def show
+    end
+
+    def new
+        @character = @world.characters.build
+    end
+
+    def create
+      
+    end
+  
+
+    def edit
+       @character = Character.find(params[:id])
+    end
+
+    def update
+    end
+
+    def destroy
+        @character = Character.find(params[:id])
+        @character.destroy
+#redirect
+    end
+
+    private 
+
+    def character_params
+        params.require(:character).permit(
+            :name,
+            :type,
+            :species,
+            :description,
+            :user_id,
+            :world_id
+        )
+    end
+
 end
