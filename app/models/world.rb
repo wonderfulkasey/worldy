8e767c9979ebf19da7670242aa95d4f710fcdcfb
wonderfulkasey <kaseyarnold15@gmail.com>
2020-10-18 18:25:35 -0400

    added stuff to world model

--- app/models/world.rb
+++ app/models/world.rb
@@ -1,7 +1,15 @@
 class World < ApplicationRecord
     #belongs_to :user
-    #has_many :characters
-    #has_many :plots
+
+    #has_many :characters, :dependent => :destroy
+
+    #has_many :plots, :dependent => :destroy
 
     validates :name, presence: true, uniqueness: true
+
+#most plots in a world
+#def self.most_plots
+#    joins(:plots).group("worlds.id").order("COUNT(*) DESC").select("worlds.*").limit(1)
+#end
+
 end
