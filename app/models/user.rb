2fae2ac67161ef1ca8f26d4bf86b793b71733b60
wonderfulkasey <kaseyarnold15@gmail.com>
2020-10-20 14:46:12 -0400

    added to user model has_many

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
+ 
+  has_many :worlds
+  accepts_nested_attributes_for :worlds
+
+  has_many :characters, through: :worlds
+  has_many :plots, through: :worlds
+
+  validates :name, length: { minimum: 3 }, uniqueness: true
+  validates :email, uniqueness: true
+ 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
